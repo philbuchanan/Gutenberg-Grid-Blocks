f25ea50e766c36d2312e3b4158ec7cdf6b3c41c9
Phil Buchanan <info@philbuchanan.com>
2019-07-04 14:15:17 -0400

    #30 Bumped version number

--- pb-grid-blocks.php
+++ pb-grid-blocks.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Grid Blocks
  * Description: Gutenberg blocks for creating responsive grid rows, columns and block grids.
- * Version: 2.0.2
+ * Version: 2.1.0
  * Author: Phil Buchanan
  * Author URI: https://philbuchanan.com
  */
