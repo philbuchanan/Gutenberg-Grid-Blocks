af6e0c830ba3437ad1051572899eca3f9288bb7d
Phil Buchanan <info@philbuchanan.com>
2020-12-07 17:10:31 -0500

    Added support for WordPress 5.6 (#67) (#68)

--- src/columns/column/settings.js
+++ src/columns/column/settings.js
@@ -3,13 +3,12 @@
  */
 import { __ } from '@wordpress/i18n';
 import { SVG, Path } from '@wordpress/components';
+import { column } from '@wordpress/icons';
 
 export default {
 	title: __('Column', 'pb'),
 
-	icon: <SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-		<Path d="M19.5,4H4.5A1.5,1.5,0,0,0,3,5.5v13A1.5,1.5,0,0,0,4.5,20h15A1.5,1.5,0,0,0,21,18.5V5.5A1.5,1.5,0,0,0,19.5,4ZM8.5,18.5H5a.5.5,0,0,1-.5-.5V6A.5.5,0,0,1,5,5.5H8.5Z" />
-	</SVG>,
+	icon: column,
 
 	category: 'layout',
 
--- src/columns/row/settings.js
+++ src/columns/row/settings.js
@@ -3,15 +3,14 @@
  */
 import { __ } from '@wordpress/i18n';
 import { SVG, Path } from '@wordpress/components';
+import { columns } from '@wordpress/icons';
 
 export default {
 	title: __('Columns', 'pb'),
 
 	description: __('Display content in multiple columns.', 'pb'),
 
-	icon: <SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-		<Path d="M19.5,4H4.5A1.5,1.5,0,0,0,3,5.5v13A1.5,1.5,0,0,0,4.5,20h15A1.5,1.5,0,0,0,21,18.5V5.5A1.5,1.5,0,0,0,19.5,4ZM8.5,18.5H5a.5.5,0,0,1-.5-.5V6A.5.5,0,0,1,5,5.5H8.5Zm5.5,0H10V5.5h4Zm5.5-.5a.5.5,0,0,1-.5.5H15.5V5.5H19a.5.5,0,0,1,.5.5Z" />
-	</SVG>,
+	icon: columns,
 
 	category: 'layout',
 
